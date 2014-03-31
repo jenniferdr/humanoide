4d8a02ec9b0b9735593a189e862f27dce539a8b7
julianaleon8 <julianaleon8@gmail.com>
2014-03-31 09:53:12 -0430

    cosas

--- SeteoMotoresYsensores/SeteoMotoresYsensores.ino
+++ SeteoMotoresYsensores/SeteoMotoresYsensores.ino
@@ -20,40 +20,19 @@ void setup(){
     delay(10);
     X = analogRead(sensorpinX);
     Y = analogRead(sensorY);
-    SetTorqueL(10, 800); 
     digitalWrite(13,LOW);
     
-    //while(q < 18){
-      //SetTorqueL(q, 800); 
-      //SetAlarm(q);
-      //q = q+1;
-    //}
+    while(q < 18){
+      SetTorqueL(q, 800); 
+      SetAlarm(q);
+      q = q+1;
+    }
     //SetTemperature(4, 30);
-    //bioloid.loadPose(pose_1);
-    //bioloid.readPose();
-    //bioloid.interpolateSetup(500);
-   // while(bioloid.interpolating > 0){
-        //bioloid.interpolateStep();
-        //delay(3);
-   // }
-   
-    // start our walking
-   // bioloid.playSeq(camina);
 }
 
 void loop(){
  /* X = analogRead(sensorpinX);
   Y = analogRead(sensorY);
-  if (X<248){
-    SetPosition(5,100);
-  } else if (X>253){
-    SetPosition(5,512);
-  } else if(Y>253){
-    SetPosition(6,100);
-  } else if (Y<248){
-    SetPosition(6,512);
-  } else {*/ 
-    //bioloid.play();    
     Serial.print(" tempe \n ");
     Temp = GetTorqueH(1);
     Serial.print(Temp);
@@ -62,5 +41,5 @@ void loop(){
     Serial.print(Temp);
     
     delay(500);
-  //}
+  }*/
 }
