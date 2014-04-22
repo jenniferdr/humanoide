ff62e955999408a8101d08567994b804b10f30d3
jenniferdr <jenniferdosreis@hotmail.com>
2014-04-22 15:04:24 -0430

    Mas poses para que no se caiga

--- caminar/caminar.ino
+++ caminar/caminar.ino
@@ -1,13 +1,23 @@
 #include <ax12.h>
 #include <BioloidController.h>
-#include "poses.h"
+#include "poses3.h"
 
 BioloidController bioloid = BioloidController(1000000);
+
+// Giroscopio
+int gyroX = 2;
+int gyroY = 0;
+
+int velocidadX;
+int velocidadY;
 
 void setup(){
   // stand up slowly
   delay(100);       // recommended pause
-  //   Serial.begin(9600);    
+  Serial.begin(9600);
+
+  pinMode(gyroX,INPUT);
+  pinMode(gyroY,INPUT);  
 
   bioloid.loadPose(pose_2);
   bioloid.readPose();
@@ -25,9 +35,13 @@ void loop(){
   bioloid.playSeq(camina);
   while(bioloid.playing){
     bioloid.play();
-  }  
-  //while(true){
-  //bioloid.play();
-  //}
+    balance();
+  } 
+}
+
+void balance(){
+
+    // Lectura promediada
+    
 }
 
