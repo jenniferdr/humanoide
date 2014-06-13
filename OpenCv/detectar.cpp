157acfca89ea3708962fb713496631586562e576
julianaleon8 <julianaleon8@gmail.com>
2014-06-13 12:18:23 -0430

    para detectar

--- OpenCv/detectar.cpp
+++ OpenCv/detectar.cpp
@@ -1,26 +1,93 @@
 #include <iostream>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include <stdio.h>
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/nonfree/nonfree.hpp"
+
+
+#include <string>   // for strings
+#include <iomanip>  // for controlling float print precision
+#include <sstream>  // string to number conversion
 
 using namespace cv;
 using namespace std;
 
+// valor de la presicion al comparar las imagenes valores 15-30 difieren en gran cantidad valores 30- infinito mas se parecen
+
+#define presicion 40 
+
+
+double getPSNR(const Mat& I1, const Mat& I2)
+{
+ Mat s1;
+ absdiff(I1, I2, s1);       // |I1 - I2|
+ s1.convertTo(s1, CV_32F);  // cannot make a square on 8 bits
+ s1 = s1.mul(s1);           // |I1 - I2|^2
+
+ Scalar s = sum(s1);         // sum elements per channel
+
+ double sse = s.val[0] + s.val[1] + s.val[2]; // sum channels
+
+ if( sse <= 1e-10) // for small values return zero
+     return 0;
+ else
+ {
+     double  mse =sse /(double)(I1.channels() * I1.total());
+     double psnr = 10.0*log10((255*255)/mse);
+     return psnr;
+ }
+}
+
+
+
+
 int main (int argc, char ** argv) {
+	// Valor del trigger 
+	int trigger = 35;
 
+	// Valor del retardo
+	int delay = 10;
+	
+	// capturar la camara en vivo 
+	// "0" es el numero del dspositivo 
+	VideoCapture camara(0);
 
-  // capturar la camara en vivo 
-  VideoCapture camara(0);
-
-  
-  
   if (!camara.isOpened()) 
     {
-      cout << "No se puedo abrir la camara" << endl;
+      cout << "No se pudo abrir la camara" << endl;
       return -1;
     }
 
+
+
+  stringstream conv;
+  
+  //const string Referencia =camara.read(imgTmp); 
+	  //"referenciaCamara.jpg";
+	  
+  
+  
+  //VideoCapture referenciaOscuro(Referencia);
+
+
+/*	 if (!referenciaOscuro.isOpened())
+    {
+        cout  << "NO SE PUEDE ABRIR IMAGEN DE REFERENCIA " << Referencia << endl;
+        return -1;
+    }
+// HAY que verificar que el tamano de la camara y de la foto sea el mismo 
+	 Size refS = Size((int) referenciaOscuro.get(CV_CAP_PROP_FRAME_WIDTH),
+					  (int) referenciaOscuro.get(CV_CAP_PROP_FRAME_HEIGHT));
+	 
+*/       
+  
   // Crea una nueva ventana
-  namedWindow("video",CV_WINDOW_AUTOSIZE);
+	 namedWindow("video",CV_WINDOW_AUTOSIZE); 
   
   /* Para calcular el HUE, Saturacion y Valor
    * de la pelota
@@ -31,11 +98,11 @@ int main (int argc, char ** argv) {
     
   int iLowS = 75; 
   int iHighS = 255;
-    
+  
   int iLowV = 78;
   int iHighV = 255;
-      
-  // Crear un control para cambiar la HUE (0 - 179)
+  
+   // Crear un control para cambiar la HUE (0 - 179)
   createTrackbar("LowH", "video", &iLowH, 179); //Hue (0 - 179)
   createTrackbar("HighH", "video", &iHighH, 179);
     
@@ -81,8 +148,9 @@ int main (int argc, char ** argv) {
 
   int iLastX = -1; 
   int iLastY = -1;
-  
-  
+  Mat imagenReferencia;
+  double valorPSNR;
+  camara.read(imagenReferencia); 
   while (1)
     {
       Mat cuadro;
@@ -91,18 +159,29 @@ int main (int argc, char ** argv) {
       Mat imgLines = Mat::zeros( imgTmp.size(), CV_8UC3 );;
       
       bool bSuccess = camara.read(imgOriginal); // leer un cuadro del video
-      
+	 
+	  //referenciaOscuro >> imagenReferencia;
       if (!bSuccess) 
 	{
 	  cout << "Cannot read a frame from video stream" << endl;
 	  break;
 	}
       Mat imgHSV;
-     
+
+///////////////////////////////// PSNR /////////////////////////////////////////////////
+
+	   valorPSNR = getPSNR(imagenReferencia,imgOriginal);
+	   cout << setiosflags(ios::fixed) << setprecision(3) << valorPSNR << "dB";
+	   if (valorPSNR > presicion){
+		   cout << "me cai hacia abajo ";
+
+	   }
+	   
       //Convertir el cuadro de BGR a HSV
       cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); 
       Mat imgFiltro;
 	  Mat imgArqueria;
+	  /********* PARA LA PELOTA  ********/
       inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgFiltro); //Threshold the image
     
       //morphological opening (removes small objects from the foreground)
@@ -148,8 +227,7 @@ int main (int argc, char ** argv) {
 	  if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
 	    {
 			line(imgLines, cvPoint(00,(imgLines.size().height)/2), cvPoint(imgLines.size().width,(imgLines.size().height)/2), cvScalar(0,255,0), 1);
-
-			//	line(imgLines, cvPoint(00,(imgLines.size().height)/2), cvPoint(imgLines.size().width,(imgLines.size().height)/2), cvScalar(0,255,0), 1);
+			//line(imgLines, cvPoint(00,(imgLines.size().height)/2), cvPoint(imgLines.size().width,(imgLines.size().height)/2), cvScalar(0,255,0), 1);
 
 			cout <<  imgTmp.size().height<< endl;   	     
 			circle(imgLines,Point2f(posX,posY),50,Scalar(255,0,0),1,CV_AA,0);
